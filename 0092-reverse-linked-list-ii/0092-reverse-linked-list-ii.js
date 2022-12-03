ec4179027cdd9a982d932a965acf3b266b077004
Sungyo (Tommy) Kim <105556961+sungyotkim@users.noreply.github.com>
2022-12-02 21:38:27 -0500

    Time: 67 ms (89.73%), Space: 42.3 MB (45.48%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.js
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.js
@@ -11,48 +11,24 @@
  * @param {number} right
  * @return {ListNode}
  */
-// function reverseBetween(head, left, right) {
-//   if (left === right || !head.next) return head;
-//   let start = head, cur = head;
-//   let i = 1;
-//   while (i < left) {
-//       start = cur;
-//       cur = cur.next;
-//       i++;
-//   }
-//   let prev = null, tail = cur;
-//   while (i <= right) {
-//       let next = cur.next;
-//       cur.next = prev;
-//       prev = cur;
-//       cur = next;
-//       i++;
-//   }
-//   start.next = prev;
-//   tail.next = cur;
-//   return head
-// };
-
-var reverseBetween = function(head, m, n) {
-    let start = head, cur = head;
-    let i = 1;
-    while (i < m) {
-        start = cur;
-        cur = cur.next;
-        i++;
-    }
-    let prev = null, tail = cur;
-    while (i <= n) {
-        let next = cur.next;
-        cur.next = prev;
-        prev = cur;
-        cur = next;
-        i++;
-    }
-    start.next = prev;
-    tail.next = cur;
-    return m == 1 ? prev : head; 
-    // if m == 1, we have no need to connect start list with reversed list reversed list itself is the start (or the head)
-    // Time Complexity: O(n)
-    // Space Complexity: O(1)
+function reverseBetween(head, left, right) {
+  // if (left === right || !head.next) return head;
+  let start = head, cur = head;
+  let i = 1;
+  while (i < left) {
+      start = cur;
+      cur = cur.next;
+      i++;
+  }
+  let prev = null, tail = cur;
+  while (i <= right) {
+      let next = cur.next;
+      cur.next = prev;
+      prev = cur;
+      cur = next;
+      i++;
+  }
+  start.next = prev;
+  tail.next = cur;
+  return left === 1 ? prev : head; 
 };
