1e3e420fbe7fb51aa53024431d90ab795a227b2f
Sungyo (Tommy) Kim <105556961+sungyotkim@users.noreply.github.com>
2022-12-09 22:10:37 -0500

    Time: 118 ms, Memory: 44.3 MB - LeetHub

--- next-greater-element-i/next-greater-element-i.js
+++ next-greater-element-i/next-greater-element-i.js
@@ -4,11 +4,16 @@
  * @return {number[]}
  */
 function nextGreaterElement(nums1, nums2) {
+  // Use a stack to track all elements smaller than initial
   const stack = [];
+  // use map to track next greatest element
   const map = {};
   let ans = [];
-  
+
+  // iterate through nums2 and add to stack if num is less than top of stack
   for (let i = 0; i < nums2.length; i++) {
+    
+    // if num is greater, pop the stack and replace the popped element in the map
     while (stack.length && nums2[i] > stack[stack.length - 1]) {
       let last = stack.pop();
       map[last] = nums2[i];
@@ -16,6 +21,7 @@ function nextGreaterElement(nums1, nums2) {
     stack.push(nums2[i])
   }
   
+  // if it exists in the map, add the answer, otherwise it's -1
   for (let i = 0; i < nums1.length; i++) {
     if (map[nums1[i]]) {
       ans.push(map[nums1[i]])
